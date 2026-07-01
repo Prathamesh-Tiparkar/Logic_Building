8d856f598171f9639fd120ab3e594d2fda767d15
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-07-02 00:27:14 +0530

    Added programs 60 to 69

---
+++ program63.c
@@ -0,0 +1,27 @@
+//INPUT : 5
+//OUTPUT  : -5 -3 -1 
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = -iNo; iCnt <= 0; iCnt += 2)
+    {
+        printf("%d\t",iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
