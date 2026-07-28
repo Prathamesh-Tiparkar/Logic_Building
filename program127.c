f3d2839b0c57d393a00d57d9d2aa7534e0e684e8
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-07-28 10:25:59 +0530

    Add program 126 to 130

---
+++ program127.c
@@ -0,0 +1,45 @@
+// Count Frequency
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountFrequency(int Arr[], int iSize)
+{
+    int iCount = 0;
+    int iCnt = 0;
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == 11)      
+        {
+            iCount ++;
+        }
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int *Brr = NULL;    
+    int iLength = 0, iCnt = 0, iRet = 0;
+
+    printf("Enter the number of Elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength);
+
+    printf("Enter the Elements: \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = CountFrequency(Brr, iLength);
+
+    printf("Count Frequency : %d \n",iRet);
+
+    free(Brr);
+    
+    return 0;
+}
