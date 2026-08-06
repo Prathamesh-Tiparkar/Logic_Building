f0736ad2ca47ef4a05075da85a9dd6809cc5c191
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-06 21:58:37 +0530

    Add program 236 to 240

---
+++ program236.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char *str = "Ganesh";       // char str[] = "Ganesh";   both the syntax are same
+    
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    return 0;
+}
