21dc0d97c88189cca5f845dfb6e92f6ae901655e
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-15 20:13:11 +0530

    Add program 336 to 340

---
+++ program340.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+// Position : 4
+
+UINT OffBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0x1;
+    UINT iResult = 0;
+
+    if(iPos < 1 || iPos > 32)       // filter
+    {
+        printf("Invalid Bit position \n");
+        return iNo;
+    }
+
+    iMask = iMask << (iPos - 1);
+
+    iMask = ~iMask;
+
+    iResult = iNo & iMask;
+
+    return iResult;
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iLocation = 0;
+    UINT iRet = 0;
+
+    printf("Enter number : \n"); 
+    scanf("%d",&iValue);
+
+    printf("Enter the bit Position: \n");
+    scanf("%d",&iLocation);
+
+    iRet = OffBit(iValue, iLocation);
+
+    printf("Updated bit is : %d \n",iRet);
+
+    return 0;
+}
