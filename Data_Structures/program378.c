ee42842aa0bbbd00fa1ef88d1b21b73e8a14b1cc
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-21 23:29:58 +0530

    LL InsertFirst, InsertLast, Count, Display func

---
+++ Data_Structures/program378.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE first)
+{
+    while(first)        // while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first)        // while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));    // node is created here after malloc
+
+    newn->data = iNo;
+    newn->next = NULL;
+    
+    // ERROR // Expression must be ==
+    if(NULL = *first ) // LinkedList is Empty
+    {
+        *first = newn;
+    }
+    else    // LL contains atleast 1 Node
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}                             
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));    // node is created here after malloc
+
+    newn->data = iNo;
+    newn->next = NULL;
+    
+    if(*first == NULL) // LinkedList is Empty
+    {
+        *first = newn;
+    }
+    else    // LL contains atleast 1 Node
+    {
+        
+    }
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{}
+
+void DeleteLast(PPNODE first)
+{}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,101);     // only this line will go to if condition
+    InsertFirst(&head,51);      // other data will goes to else becoz it is empty
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    iRet = Count(head);
+
+    printf("Number of Nodes are : %d \n",iRet);
+
+    return 0;
+}
