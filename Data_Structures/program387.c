8de4885497a2d7922e4e9ef3cb2a11c4e8d9f274
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-23 17:35:36 +0530

    Doubly Linked List

---
+++ Data_Structures/program387.c
@@ -0,0 +1,89 @@
+// Doubly LL
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;              // $
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+void Display(PNODE first)
+{}
+
+void InsertFisrt(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data =iNo;
+    newn->next = NULL;
+    newn->prev = NULL;              // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        (*first)->prev = newn;          // $
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data =iNo;
+    newn->next = NULL;
+    newn->prev = NULL;              // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        
+    }
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{}
+
+void DeleteLast(PPNODE first)
+{}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertFisrt(&head, 51);
+    InsertFisrt(&head, 21);
+    InsertFisrt(&head, 11);
+
+    
+
+    return 0;
+}
