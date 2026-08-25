ba488044802afe6109810f4d6b26958bb4437137
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-25 21:24:49 +0530

    DLL DeleteFirst and DeleteLast

---
+++ Data_Structures/program392.c
@@ -0,0 +1,174 @@
+// Doubly LL
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;              // $
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+}
+
+void Display(PNODE first)
+{
+    printf("\n NULL <=> ");
+
+    while(first != NULL)
+    {
+        printf("| %d | <=> ",first->data);
+        first = first -> next;
+    }
+
+    printf("NULL \n");
+}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data =iNo;
+    newn->next = NULL;
+    newn->prev = NULL;              // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        (*first)->prev = newn;          // $
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data =iNo;
+    newn->next = NULL;
+    newn->prev = NULL;              // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;          // 100
+        
+        while(temp -> next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn;
+        newn->prev = temp;      // $
+    }
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{
+    if(*first == NULL)
+    {
+        return;
+    }
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        *first = (*first) -> next;
+        free((*first)->prev);      // $
+        (*first)->prev = NULL;      // $
+    }
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        temp = *first;
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+
+    InsertLast(&head,101);
+    InsertLast(&head,111);
+    InsertLast(&head,121);
+
+    Display(head);
+    int iRet = Count(head);
+    printf("Number of Elements are: %d \n",iRet);
+
+    DeleteFirst(&head);
+
+    Display(head);
+    int iRet = Count(head);
+    printf("Number of Elements are: %d \n",iRet);
+    
+    DeleteLast(&head);
+
+    Display(head);
+    int iRet = Count(head);
+    printf("Number of Elements are: %d \n",iRet);
+
+    return 0;
+}
