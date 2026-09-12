8b3bd0af1c2819c15770757b29a292881da5f786
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-12 10:12:42 +0530

    Add files via upload

---
+++ Data_Structures/program441.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+#pragma pack(1)
+class DoublyCl
+{
+    private:
+        PNODE first;
+        PNODE last;
+        int iCount;
+    public:
+
+};
+
+int main()
+{
+    DoublyCl dobj;
+
+    cout<<sizeof(dobj)<<"\n";
+
+    return 0;
+}
