93103947b42dbea52c3222fdd45777727a08ea60
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-17 22:49:04 +0530

    Add files via upload

---
+++ Data_Structures/program448.java
@@ -0,0 +1,46 @@
+// LL created explicitly in java
+
+class node
+{
+    public int data;
+    public node next;
+}
+
+class program448
+{
+    public static void main(String A[])
+    {
+        node head = null;
+
+        node obj1= null;        // reference created
+        node obj2 = null;
+        node obj3 = null;
+
+        obj1 = new node();      // memory allocated
+        obj2 = new node();
+        obj3 = new node();
+
+        obj1.data = 11;
+        obj2.data = 21;
+        obj3.data = 51;
+
+        obj1.next = obj2;       // LL created 
+        obj2.next = obj3;
+        obj3.next = null;
+
+        head = obj1;            
+
+        node temp = head;
+
+        System.out.println(temp.data);
+        
+        temp = temp.next;
+        System.out.println(temp.data);
+
+        temp = temp.next;
+        System.out.println(temp.data);
+
+        temp = temp.next;       // reached to null
+    }
+    
+}
